bd727e2625f87a20d46179fdde44b80d79db4db8
Daniel Berlin <dberlin@dberlin.org>
2022-04-20 02:00:41 -0400

    Make this work properly with TrueNAS

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM ubuntu:21.10
 
 # Maintainer
-LABEL maintainer "Alexander Graf <alex@otherguy.io>"
+LABEL maintainer "Daniel Berlin <dberlin@dberlin.org>"
 
 # Required to prevent warnings
 ARG DEBIAN_FRONTEND=noninteractive
@@ -28,9 +28,9 @@ RUN apt-get update \
    libxcb-sync1 libxshmfence1 libxxf86vm1 python3-gpg
 
 # Create user and group
-RUN mkdir -p /opt/dropbox /opt/dropbox/.dropbox /opt/dropbox/Dropbox \
- && useradd --home-dir /opt/dropbox --comment "Dropbox Daemon Account" --user-group --shell /usr/sbin/nologin dropbox \
- && chown -R dropbox:dropbox /opt/dropbox
+RUN mkdir -p /home/dropbox \
+ && useradd --home-dir /home/dropbox --comment "Dropbox Daemon Account" --user-group --shell /usr/sbin/nologin dropbox \
+ && chown -R dropbox:dropbox /home/dropbox
 
 # https://help.dropbox.com/installs-integrations/desktop/linux-repository
 RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FC918B335044912E \
@@ -40,23 +40,8 @@ RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FC918B335044912E \
  && apt-get -qqy autoclean \
  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# Dropbox insists on downloading its binaries itself via 'dropbox start -i'
-RUN echo "y" | gosu dropbox dropbox start -i
-
-# Dropbox has the nasty tendency to update itself without asking. In the processs it fills the
-# file system over time with rather large files written to /opt/dropbox/ and /tmp.
-#
-# https://bbs.archlinux.org/viewtopic.php?id=191001
-RUN mkdir -p /opt/dropbox/bin/ /tmp \
- && mv /opt/dropbox/.dropbox-dist/* /opt/dropbox/bin/ \
- && rm -rf /opt/dropbox/.dropbox-dist \
- && install -dm0 /opt/dropbox/.dropbox-dist \
- && chmod u-w /opt/dropbox/.dropbox-dist \
- && chmod o-w /tmp \
- && chmod g-w /tmp
-
 # Create volumes
-VOLUME ["/opt/dropbox/.dropbox", "/opt/dropbox/Dropbox"]
+VOLUME ["/home/dropbox"]
 
 # Build arguments
 ARG VCS_REF=main
@@ -75,7 +60,7 @@ LABEL org.label-schema.vcs-ref        "${VCS_REF}"
 # Configurable sleep delay
 ENV POLLING_INTERVAL=5
 # Possibility to skip permission check
-ENV SKIP_SET_PERMISSIONS=false
+ENV SKIP_SET_PERMISSIONS=true
 
 # Install init script and dropbox command line wrapper
 COPY docker-entrypoint.sh /
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,51 +42,9 @@ usermod -u ${DROPBOX_UID} -g ${DROPBOX_GID} --non-unique dropbox > /dev/null 2>&
 # Change ownership to dropbox account on all working folders.
 if [[ $(echo "${SKIP_SET_PERMISSIONS:-false}" | tr '[:upper:]' '[:lower:]' | tr -d " ") == "true" ]]; then
   echo "Skipping permissions check, ensure the dropbox user owns all files!"
-  chown ${DROPBOX_UID}:${DROPBOX_GID} /opt/dropbox
+  chown ${DROPBOX_UID}:${DROPBOX_GID} /home/dropbox
 else
-  chown -R ${DROPBOX_UID}:${DROPBOX_GID} /opt/dropbox
-fi
-
-# Change permissions on Dropbox folder
-chmod 755 /opt/dropbox/Dropbox
-
-#  Dropbox did not shutdown properly? Remove files.
-[ ! -e "/opt/dropbox/.dropbox/command_socket" ] || rm /opt/dropbox/.dropbox/command_socket
-[ ! -e "/opt/dropbox/.dropbox/iface_socket" ]   || rm /opt/dropbox/.dropbox/iface_socket
-[ ! -e "/opt/dropbox/.dropbox/unlink.db" ]      || rm /opt/dropbox/.dropbox/unlink.db
-[ ! -e "/opt/dropbox/.dropbox/dropbox.pid" ]    || rm /opt/dropbox/.dropbox/dropbox.pid
-
-# Update Dropbox to latest version unless DROPBOX_SKIP_UPDATE is set
-if [[ -z "$DROPBOX_SKIP_UPDATE" ]]; then
-  echo "Checking for latest Dropbox version..."
-  sleep 1
-
-  # Get download link for latest dropbox version
-  DL=$(curl -I -s https://www.dropbox.com/download/\?plat\=lnx.x86_64 | grep ocation | awk -F'ocation: ' '{print $2}')
-
-  # Strip CRLF
-  DL=${DL//[$'\t\r\n ']}
-
-  # Extract version string
-  Latest=$(echo $DL | sed 's/.*x86_64-\([0-9]*\.[0-9]*\.[0-9]*\)\.tar\.gz/\1/')
-
-  # Get current Version
-  Current=$(cat /opt/dropbox/bin/VERSION)
-  echo "Latest   :" $Latest
-  echo "Installed:" $Current
-  if [ ! -z "${Latest}" ] && [ ! -z "${Current}" ] && [ $Current != $Latest ]; then
-  	echo "Downloading Dropbox $Latest..."
-  	tmpdir=`mktemp -d`
-  	curl -# -L $DL | tar xzf - -C $tmpdir
-  	echo "Installing new version..."
-  	rm -rf /opt/dropbox/bin/*
-  	mv $tmpdir/.dropbox-dist/* /opt/dropbox/bin/
-  	rm -rf $tmpdir
-    find /opt/dropbox/bin/ -type f -name "*.so" -exec chown ${DROPBOX_UID}:${DROPBOX_GID} {} \; -exec chmod a+rx {} \;
-  	echo "Dropbox updated to v$Latest"
-  else
-    echo "Dropbox is up-to-date"
-  fi
+  chown -R ${DROPBOX_UID}:${DROPBOX_GID} /home/dropbox
 fi
 
 # Empty line
@@ -100,16 +58,7 @@ echo "Using $(cat /etc/timezone) timezone ($(date +%H:%M:%S) local time)"
 dpkg-reconfigure --frontend noninteractive tzdata
 
 # Start Dropbox
-echo "Starting dropboxd ($(cat /opt/dropbox/bin/VERSION))..."
-gosu dropbox "$@" & export DROPBOX_PID="$!"
-trap "/bin/kill -SIGQUIT ${DROPBOX_PID}" INT
-
-# Wait a few seconds for the Dropbox daemon to start
-sleep 5
-
-# Dropbox likes to restart itself. In that case, the container will exit!
-while kill -0 ${DROPBOX_PID} 2> /dev/null; do
-  [ -d "/proc/${DROPBOX_PID}" ] && [ -f "/opt/dropbox/.dropbox/info.json" ] && gosu dropbox dropbox status
-  /usr/bin/find /tmp/ -maxdepth 1 -type d -mtime +1 ! -path /tmp/ -exec rm -rf {} \;
-  /bin/sleep ${POLLING_INTERVAL}
-done
+echo "Starting dropbox"
+echo "y" | gosu dropbox dropbox start -i
+#gosu dropbox dropbox start
+sleep infinity
